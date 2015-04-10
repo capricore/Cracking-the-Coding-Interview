cf6b00b4869370623d234073754b38c326e9520e
capricore <405020841@qq.com>
2015-04-10 15:54:46 +0800

    reverse a string
    Signed-off-by: capricore <405020841@qq.com>

---
+++ src/ArraysandStrings/Reverse.java
@@ -0,0 +1,22 @@
+package ArraysandStrings;
+
+import java.util.Scanner;
+
+public class Reverse {
+	String reverse(String string){
+		int n = string.length();
+		char[] c = string.toCharArray();
+		for (int i = 0; i <= n/2; i++) {
+			char temp = c[i];
+			c[i] = c[n-1-i];
+			c[n-1-i] = temp;
+		}
+		return new String(c);
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String string = scanner.next();
+		System.out.println(new Reverse().reverse(string));
+	}
+}
